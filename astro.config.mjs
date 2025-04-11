8880df1a803ef0066e30fe0d08cf6c4ff3a4b5a0
Marcos Diaz <Diazelhalabimarcosa@gmail.com>
2025-04-11 10:21:52 -0400

    Se completo parte de la pantalla principal

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,30 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true,
-    build: {
-        // Genera nombres de archivo predecibles para el cache
-        assetsPrefix: '/',
-        inlineStylesheets: 'never'
-    },
-    vite: {
+  prefetch: true,
 
-        plugins: [tailwindcss()],
-        build: {
-            rollupOptions: {
-                output: {
-                    assetFileNames: 'assets/[name].[hash][extname]',
-                    entryFileNames: 'js/[name].[hash].js'
-                }
-            }
-        }
-    }
+  build: {
+      // Genera nombres de archivo predecibles para el cache
+      assetsPrefix: '/',
+      inlineStylesheets: 'never'
+  },
+
+  vite: {
+
+      plugins: [tailwindcss()],
+      build: {
+          rollupOptions: {
+              output: {
+                  assetFileNames: 'assets/[name].[hash][extname]',
+                  entryFileNames: 'js/[name].[hash].js'
+              }
+          }
+      }
+  },
+
+  integrations: [react()]
 });
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "0.0.1";
+const VERSION = "0.0.2";
 const CACHE_NAME = `hb-finances-${VERSION}`;
 
 const ASSETS_TO_CACHE = [
